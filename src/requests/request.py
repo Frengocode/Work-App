2d0eeaec63ef735995b371ac017175ea410360d7
Frengocode <wendypython17@gmail.com>
2024-10-25 14:12:18 +0500

    Fixed All

---
+++ src/requests/request.py
@@ -0,0 +1,6 @@
+AUTH_REQUEST = "http://192.168.100.59:8000/auth-login/"
+GET_CURRENT_USER_REQUEST = (
+    "http://192.168.100.59:8000/user-service/api/v1/get-current-user"
+)
+
+GET_USER_REQUEST = "http://192.168.100.59:8000/user-service/api/v1/get-user/"
