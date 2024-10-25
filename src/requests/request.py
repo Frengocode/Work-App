2f64e521e5a216f76e7a199a77577d9adf2e5da2
Frengocode <164409199+Frengocode@users.noreply.github.com>
2024-10-25 14:42:59 +0500

    Update request.py

--- src/requests/request.py
+++ src/requests/request.py
@@ -1,6 +1,6 @@
-AUTH_REQUEST = "http://192.168.100.59:8000/auth-login/"
+AUTH_REQUEST = "yourport/auth-login/"
 GET_CURRENT_USER_REQUEST = (
-    "http://192.168.100.59:8000/user-service/api/v1/get-current-user"
+    "yourport/user-service/api/v1/get-current-user"
 )
 
-GET_USER_REQUEST = "http://192.168.100.59:8000/user-service/api/v1/get-user/"
+GET_USER_REQUEST = "yourport/user-service/api/v1/get-user/"
